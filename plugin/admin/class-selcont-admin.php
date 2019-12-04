f5a332e0fecf83787c60f8bce666d6cdbcc6941d
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2019-12-04 13:11:35 +0200

    Update class-selcont-admin.php

--- plugin/admin/class-selcont-admin.php
+++ plugin/admin/class-selcont-admin.php
@@ -479,7 +479,8 @@ class Selcont_Admin {
         $repeatable_fields = get_post_meta($post->ID, 'repeatable_fields', true);
 
         if ( empty( $repeatable_fields ) ){
-            $repeatable_fields[] = array (
+	$repeatable_fields = [];
+        $repeatable_fields [] = array (
                 'stt_meta_box_image' => '',
                 'stt_meta_box_title' => '',
                 'stt_meta_box_time' => '' );
